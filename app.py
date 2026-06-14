62abb806b719c526b039eed5d0a935ad1003a461
abel-111 <sng23cs001@sngce.ac.in>
2026-06-14 18:46:50 +0530

    Add form to create expenses

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 app = Flask(__name__)
+expenses = []
 @app.route('/')
 def home():
-    expenses = [
-        {"category": "Food", "amount": 250},
-        {"category": "Travel", "amount": 100},
-        {"category": "Shopping", "amount": 500},
-    ]
     return render_template('home.html',username='Abel',expenses=expenses)
+@app.route('/add',methods=['POST'])
+def expense():
+    category = request.form['category']
+    amount = request.form['amount']
+    expenses.append({'category': category, 'amount': amount})
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':    app.run(debug=True)
